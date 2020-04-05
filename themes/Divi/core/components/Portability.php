d89dc4ddef7556c5b3c79dad10f2d2417798c7f6
Landon Wiedenman <landon@wiedenman.com>
2020-04-05 14:03:10 -0500

    print upload array

--- themes/Divi/core/components/Portability.php
+++ themes/Divi/core/components/Portability.php
@@ -110,7 +110,7 @@ class ET_Core_Portability {
 			 * @param string $file The absolute path to the uploaded JSON file's temporary location.
 			 */
 			do_action( 'et_core_portability_import_file', $upload['file'] );
-            error_log("PORTABILITY upload: " . $upload);
+            error_log("PORTABILITY upload: " . print_r($upload));
 
 			$temp_file = $this->temp_file( $temp_file_id, 'et_core_import', $upload['file'] );
 			$import = json_decode( $filesystem->get_contents( $temp_file ), true );
