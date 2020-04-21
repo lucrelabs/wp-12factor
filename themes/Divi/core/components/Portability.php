8b504b0bf43defa659142173fbdba9bda932e7e1
Landon Wiedenman <landon@wiedenman.com>
2020-04-21 12:11:36 -0500

    Add test log

--- themes/Divi/core/components/Portability.php
+++ themes/Divi/core/components/Portability.php
@@ -111,6 +111,7 @@ class ET_Core_Portability {
 			 */
 			do_action( 'et_core_portability_import_file', $upload['file'] );
             error_log("PORTABILITY upload: " . print_r($upload));
+            error_log('test landon');
 
 			$temp_file = $this->temp_file( $temp_file_id, 'et_core_import', $upload['file'] );
 			$import = json_decode( $filesystem->get_contents( $temp_file ), true );
